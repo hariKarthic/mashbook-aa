fcfa90f42accbdc18e38f25775994961ae23737f
Abraham Jagadeesh <ajagadeesh@sapient.com>
2017-04-22 18:31:34 +0530

    Client side notification

--- src/app/services/push-notification.service.ts
+++ src/app/services/push-notification.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { FirebaseApp, AngularFire } from "angularfire2";
 import * as firebase from 'firebase';
+import axios from 'axios';
 
 @Injectable()
 export class PushNotificationService {
@@ -17,6 +18,31 @@ export class PushNotificationService {
         return this.messaging.getToken();
       }).then((token) => {
         console.log("has persmission: ", token);
+        const params = {
+          "registration_token": token,
+          "notification_topic": "my_journal"
+        };
+
+        axios({
+          url: "https://scrapbook-mash.herokuapp.com/notification/subscribe",
+          method: "post",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            "registration_token": token,
+            "notification_topic": "my_journal"
+          }
+        }).then(function (response) {
+
+
+          console.log("[RESPONSE]: ", response);
+
+        })
+          .catch(function (error) {
+            console.log("[ERROR]: ", error);
+
+          });
         // alert(token);
       }).catch(err => { console.log(err) });
   }

ace3befada1415bcf1a17d19b9275ded47cf56a3
Mahesh <mahesh.ambiga@gmail.com>
2017-04-23 21:56:22 +0530

    Added timestamp to the gallery screen.

--- src/app/models/card.model.ts
+++ src/app/models/card.model.ts
@@ -4,14 +4,15 @@ export class Card {
 	caption: string;
 	src: string;
 	selectedFilter: string;
+	type:string;
 
-	constructor(id:string, createdAt: number, caption: string, src: string, filter:string) {
+	constructor(type:string, id:string, createdAt: number, caption: string, src: string, filter:string) {
 		this.id = id;
 		this.createdAt = createdAt;
 		this.caption = caption;
 		this.src = src;
 		this.selectedFilter = filter;
-	
+	  this.type = type;
 	}
 }
 

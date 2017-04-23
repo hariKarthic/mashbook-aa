9b7717765de7eb988b0d47d83f53aa1e08bfb40b
Abraham Jagadeesh <ajagadeesh@sapient.com>
2017-04-23 11:45:23 +0530

    base64 to blob

--- src/app/components/galleryview/galleryview.component.ts
+++ src/app/components/galleryview/galleryview.component.ts
@@ -39,7 +39,7 @@ export class GalleryviewComponent implements OnInit, AfterViewInit {
   getCards() {
     this.StorageService.getData(this.Constants.cards)
     .then((resp:any)=> {
-      if(resp.length>0){
+      if(!resp){  
         this.cards = resp;
         this.loadMasonry();
         this.isLoading = false;
---
+++ src/app/components/page-loader/page-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sb-page-loader',
+  templateUrl: './page-loader.component.html',
+  styleUrls: ['./page-loader.component.scss']
+})
+export class PageLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

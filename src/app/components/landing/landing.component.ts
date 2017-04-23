8261bdb8104e93e42e36419d62312ad6a78a2010
Shalini Ja <jha.shalini.dev@gmail.com>
2017-04-24 01:37:26 +0530

    Fixes for camera icon in home screen

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -24,9 +24,7 @@ export class LandingComponent implements OnInit {
     /*navigate to gallery after 2 secs...*/
 
     setTimeout(() => {
-
-     // this.router.navigate(["/gallery"]);
-
+      this.router.navigate(["/gallery"]);
     }, 4000);
 
   }
